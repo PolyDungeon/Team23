1ad3024ffac3438357b0009a2679e0e806c72dba
Trate <trate.underwood@hotmail.com>
2023-09-24 16:04:12 -0400

    Base About Page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React,{ useState, useEffect} from 'react'
 
 const About = () => {
+    //URL will be updated when we have a working database
+    const url = "temp";
+    const [data, setData] = useState([]);
+
+    const fetchInfo = () =>{
+        return fetch(url)
+            .then((res) => res.json())
+            .then((d) => setData(d))
+    }
+
+    useEffect(() => {
+        fetchInfo();
+    }, []);
+
+    
     return (
-        <div>
-            <h1>About Us</h1>
-            <p>This is the About Us page content.</p>
+        <div className="content">
+            
+            <h1>TruckBucks</h1>
+                <p>Team: {}</p>
+                <p>Version: {}</p>
+                <p>Release Date: {}</p>
+                <h5>What is TruckBucks?</h5>
+                <p>...</p>
+            
         </div>
     );
 }
