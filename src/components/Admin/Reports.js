4662874380eb3710491ede68b7026b0736e7d9cc
Trate <trate.underwood@hotmail.com>
2023-10-05 17:08:56 -0400

    Start adding options on Report page

--- src/components/Admin/Reports.js
+++ src/components/Admin/Reports.js
@@ -1,14 +1,88 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useState } from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 
-export default class Reports extends Component{
-    render(){
+const Reports = () =>{
+
+    const initialFormData = {
+        name: '',
+        formType: 'salesBySponsor',
+        reportSize: ' '
+    };
+    
+    const [formData, setFormData] = useState(initialFormData);
+    const [startDate,setStartDate] = useState(new Date());
+    const [endDate,setEndDate] = useState(new Date());
+    
+    const handleInputChange = (event) => {
+        const { name, value} = event.target;
+        setFormData({ ...formData, [name]: value});
+    };
+    
 
         return(
+
         <div className="content">
         <h1>Reports</h1>
+        <div>
+            <label>Report Type:</label>
+            &nbsp;
+            <select
+                name="formType"
+                value={formData.formType}
+                onChange={handleInputChange}
+            >
+                <option value="salesBySponsor">Sponsors</option>
+                <option value="salesByDriver">Drivers</option>
+                <option value="invoice">Invoice</option>
+                <option value="auditlog">Audit Log</option>
+            </select>
+        </div>
+            
+        <div>
+            {formData.formType === 'salesBySponsor' && (
+               <select
+                    name="reportSize"
+                    value={formData.reportSize}
+                    onChange={handleInputChange}
+                    >
+                        <option value="all">All Sponsors</option>
+                        <option value="one">One Sponsor</option>
+                    </select>
+               
+            )}
+        </div>
+        <div>
+            <label> Date Range: </label>
+            &nbsp;
+            <DatePicker 
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            maxDate={endDate}
+            />
+            <>:</>
+            <DatePicker 
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+            minDate={startDate}
+            />
+        
+        </div>
+        <div>
+            <button>Create Report</button>
+        </div>
+            
+
+
+
         </div>
         )
-    }
-}
+
+};
+export default Reports;
+
+
+
--- src/components/Admin/createAdmin.js
+++ src/components/Admin/createAdmin.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
+
 
 
 
@@ -7,7 +8,34 @@ export default class createAdmin extends Component{
 
         return(
         <div className="content">
+        
         <h1>Create an Admin User</h1>
+        
+
+        <p>Email:  
+            <input
+                type="email"
+                name="adminEmail"
+                placeholder="example@email.com"
+            />
+        </p>
+        <p>Temporary Password:
+            <input
+                type="password"
+                name="adminTempPassword"
+                placeholder="Enter Password"
+            />
+        </p>
+        <p>Confirm Password:
+            <input
+                type="password"
+                name="adminTempPassword"
+                placeholder="Enter Password"
+            />
+        </p>
+        
+        <button>Create New Account</button>
+
         </div>
         )
     }
