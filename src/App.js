821307449dec814ef021d7dcce0ba0e68263bb2b
Vursteg <garrett.versteegh@gmail.com>
2023-10-19 22:24:11 -0400

    Fixed filename error

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ import About from './components/About';
 import DriverApp from './components/Driver/DriverApp';
 import AdminProfile from './components/Admin/AdminProfile';
 import ItemsPage from './components/Products/FindItems';
-import AdminReports from './components/Admin/AdminReports';
-import createAdmin from './components/Admin/CreateAdmin';
-import createSponsorOrg from './components/Admin/CreateSponsorOrg';
+import AdminReports from './components/Admin/adminReports';
+import createAdmin from './components/Admin/createAdmin';
+import createSponsorOrg from './components/Admin/createSponsorOrg';
 import Login from './components/Login';
 
 
