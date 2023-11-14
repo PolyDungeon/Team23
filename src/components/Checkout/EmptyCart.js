3c46e17d027e8d8dd18379b08992178e4a779042
Parker Lee Hundley <phundle@clemson.edu>
2023-11-14 18:58:56 +0000

    Updated EmptyCart.js

--- src/components/Checkout/EmptyCart.js
+++ src/components/Checkout/EmptyCart.js
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
 export default function EmptyCart() {
     return (
+        
         <div className="container mt-5">
             <div className="row">
                 <div className="col-10 mx-auto text-center text-title">
                   <h1>Your cart is empty</h1> 
+                  <Link to="/purchasehistory" className="text-uppercase">
+                        Purchase History
+                    </Link>
                </div>
             </div>
         </div>
